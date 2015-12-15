447a4d911cd9a58dc47bd6e03b4bd97dfcf5432e
malchun <zmalchunz@gmail.com>
2015-12-15 19:34:44 +0300

    This is still not working

--- src/main/java/com/itdhq/infavoritesassociation/InFavoritesAssociation.java
+++ src/main/java/com/itdhq/infavoritesassociation/InFavoritesAssociation.java
@@ -2,8 +2,13 @@ package com.itdhq.infavoritesassociation;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.favourites.FavouritesServiceImpl;
+import org.alfresco.repo.policy.Behaviour;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 
 /**
@@ -16,12 +21,19 @@ public class InFavoritesAssociation
     private Logger logger = Logger.getLogger(InFavoritesAssociation.class);
 
     private NodeService nodeService;
+    private PolicyComponent policyComponent;
 
     public void setNodeService(NodeService nodeService) { this.nodeService = nodeService; }
+    public void setPolicyComponent(PolicyComponent policyComponent) {this.policyComponent = policyComponent; }
 
     public void init()
     {
         logger.debug("InFavoritesAssociation set to work!");
+        Behaviour onAddFavourite = new JavaBehaviour(this, "onAddFavourite", Behaviour.NotificationFrequency.TRANSACTION_COMMIT);
+        this.policyComponent.bindClassBehaviour(QName.createQName(NamespaceService.ALFRESCO_URI, "onAddFavourite"), InFavoritesAssociation.class, onAddFavourite);
+
+        Behaviour onRemoveFavourite = new JavaBehaviour(this, "onRemoveFavourite", Behaviour.NotificationFrequency.TRANSACTION_COMMIT);
+        this.policyComponent.bindClassBehaviour(QName.createQName(NamespaceService.ALFRESCO_URI, "onRemoveFavourite"), InFavoritesAssociation.class, onRemoveFavourite);
     }
 
     @Override
